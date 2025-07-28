b34033080ba63ef8c9a9e6533e9e7c5eac7dfbbd
Keoma Ricci De Lima <kricci-d@z1r5p2.42lyon.fr>
2025-07-28 12:40:27 +0200

    merge commit

--- objects/parsing/Client.d
+++
@@ -1,8 +0,0 @@
-objects/parsing/Client.o: srcs/parsing/Client.cpp srcs/parsing/Client.hpp \
-  srcs/parsing/HttpRequest.hpp srcs/parsing/HttpResponse.hpp
-
-srcs/parsing/Client.hpp:
-
-srcs/parsing/HttpRequest.hpp:
-
-srcs/parsing/HttpResponse.hpp:
